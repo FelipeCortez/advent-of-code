49078db059517b3831dc2e141fb7b86477dc6d51
Felipe Cortez <felipecortezfi@gmail.com>
2018-12-02 16:39:10 -0200

    Day 2

---
+++ 2.1.clj
@@ -0,0 +1,11 @@
+(ns advent.checksum)
+
+(defn two-and-three [str]
+  (let [counts (->> str sort (partition-by identity) (map count))]
+    (map #(some (partial = %) counts) [2 3])))
+
+(let [lines (clojure.string/split-lines (slurp "2.in"))]
+  (let [twos-and-threes (map two-and-three lines)]
+    (println (* (count (filter identity (map first twos-and-threes)))
+                (count (filter identity (map second twos-and-threes))))))
+)
---
+++ 2.2.clj
@@ -0,0 +1,21 @@
+(ns advent.rightbox)
+
+(defn all-pairs [coll]
+  (when-let [s (next coll)]
+    (lazy-cat (for [y s] [(first coll) y])
+              (all-pairs s))))
+
+(defn same-and-different [x y]
+  (let [pairs (map vector x y)]
+    (letfn [(alleq [pair] (apply = pair))]
+      {:same      (->> (filter alleq pairs)
+                       (map first)
+                       (apply str))
+       :different (remove alleq pairs)})))
+
+(let [lines (clojure.string/split-lines (slurp "2.in"))]
+  (println (->> (all-pairs lines)
+                (map (partial apply same-and-different))
+                (filter #(= 1 (count (:different %))))
+                first
+                :same)))
