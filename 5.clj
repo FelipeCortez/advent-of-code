1ce98ff8ddb237a389b52d539b76d19af8b5b2ba
Felipe Cortez <felipecortezfi@gmail.com>
2018-12-05 14:08:57 -0200

    Day 5

---
+++ 5.clj
@@ -0,0 +1,36 @@
+(ns alchemical-reduction)
+
+;; part 1
+(def case-distance (- (int \A) (int \a)))
+
+(defn eq-upper-or-lower [x y]
+  (or (= (+ (int x) case-distance) (int y))
+      (= (- (int x) case-distance) (int y))))
+
+(def polymer (-> "5.in" slurp clojure.string/trim-newline))
+
+(defn react [polymer]
+  (reduce (fn [coll x]
+            (if (and (seq coll) (eq-upper-or-lower x (last coll)))
+              (vec (butlast coll))
+              (conj coll x)))
+          []
+          polymer))
+
+(count (react polymer))
+
+;; part 2
+(defn lowercase-eq [& args]
+  (apply = (map clojure.string/lower-case args)))
+
+(defn remove-char [char string]
+  (remove #(lowercase-eq char %) string))
+
+(def a-to-z (map char (range (int \a) (inc (int \z)))))
+
+;; waaaaaay too slow
+(min (map #(->> polymer
+                         (remove-char %)
+                         react
+                         count)
+                   a-to-z))
