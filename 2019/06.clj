8629214a20d7a279d41321bbb653f1a1d6eadfd4
FelipeCortez <felipecortezfi@gmail.com>
2019-12-07 11:20:51 -0300

    Day 6, part 2
    very ugly solution tho

--- 2019/06.clj
+++ 2019/06.clj
@@ -1,6 +1,8 @@
 (ns universal-orbit-map
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojure.set]))
 
+;;; part 1
 (def orbit-map (->> (slurp "06.in")
                     (str/split-lines)
                     (map (fn [s] (map keyword (str/split s #"\)"))))
@@ -15,3 +17,26 @@
 
 (let [objects (set (keys orbit-map))]
   (apply + (map (partial orbits-number orbit-map) objects)))
+
+;;; part 2
+(def orbit-graph (->> (slurp "06.in")
+                      (str/split-lines)
+                      (map (fn [s] (map keyword (str/split s #"\)"))))
+                      (reduce (fn [m [orbited orbits]] (-> m
+                                                           (update orbits #(conj % orbited))
+                                                           (update orbited #(conj % orbits))))
+                              {})))
+
+(defn shortest [orbit-graph current visited to path]
+  (let [visitable (clojure.set/difference (set (current orbit-graph)) visited)]
+    (cond
+      (= current to)        (do (println (count path) (- (count path) 3) path) path)
+      (not (seq visitable)) nil
+      :else (reduce (fn [coll object]
+                      (if-let [full-path (shortest orbit-graph object (conj visited object) to (conj path object))]
+                        (conj coll full-path)
+                        coll))
+                    []
+                    visitable))))
+
+(shortest orbit-graph :YOU #{} :SAN [:YOU])
