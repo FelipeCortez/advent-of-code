6230f5bd3b462d8e29043e2b48d899f8afdc0ab5
FelipeCortez <felipecortezfi@gmail.com>
2019-12-07 14:36:10 -0300

    iterate-based solution for Day 6
    ripped from other Clojure Advent of Code repos. I'm not a clever man

--- 2019/06.clj
+++ 2019/06.clj
@@ -18,24 +18,7 @@
   (apply + (map (partial orbits-number orbit-map) objects)))
 
 ;;; part 2
-(def orbit-graph (->> (slurp "06.in")
-                      (str/split-lines)
-                      (map (fn [s] (map keyword (str/split s #"\)"))))
-                      (reduce (fn [m [orbited orbits]] (-> m
-                                                           (update orbits #(conj % orbited))
-                                                           (update orbited #(conj % orbits))))
-                              {})))
-
-(defn shortest [orbit-graph current visited to path]
-  (let [visitable (clojure.set/difference (set (current orbit-graph)) visited)]
-    (cond
-      (= current to)        (do (println (count path) (- (count path) 3) path) path) ; this feels like cheating, I hate it
-      (not (seq visitable)) nil
-      :else (reduce (fn [coll object]
-                      (if-let [full-path (shortest orbit-graph object (conj visited object) to (conj path object))]
-                        (conj coll full-path)
-                        coll))
-                    []
-                    visitable))))
-
-(shortest orbit-graph :YOU #{} :SAN [:YOU])
+(- (count (filter (fn [[object freq]] (= 1 freq))
+                  (frequencies (concat (take-while #(not= :COM %) (iterate orbit-map :YOU))
+                                       (take-while #(not= :COM %) (iterate orbit-map :SAN))))))
+   2)
