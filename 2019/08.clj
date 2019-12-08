b74b810d0edae95425ea313aa9f1537fb0ee0293
FelipeCortez <felipecortezfi@gmail.com>
2019-12-08 16:21:37 -0300

    Day 8, part 2
    easy!

--- 2019/08.clj
+++ 2019/08.clj
@@ -5,6 +5,7 @@
 
 (def input (-> (slurp "08.in") str/trim))
 
+;;; part 1
 (->> input
      (map to-int)
      (partition (* 25 6))
@@ -12,3 +13,11 @@
      (apply min-key (fn [m] (get m 0)))
      ((fn [m] (* (get m 1) (get m 2)))))
 
+;;; part 2
+(->> input
+     (map to-int)
+     (partition (* 25 6))
+     (apply map vector)
+     (map #(reduce (fn [_ n] (when-not (= 2 n) (reduced n))) 0 %))
+     (partition 25)
+     (map (comp println #(str/replace % "1" "X") #(str/replace % "0" " ") str/join)))
