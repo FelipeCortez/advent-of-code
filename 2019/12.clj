b04d119074686e7dc9365bc9df95fe96689f209e
FelipeCortez <felipecortezfi@gmail.com>
2019-12-16 21:04:05 -0300

    Day 12, part 2

--- 2019/12.clj
+++ 2019/12.clj
@@ -6,13 +6,6 @@
                 (mapv (fn [line] {:velocity [0 0 0]
                                   :position (mapv read-string (re-seq #"-?\d+" line))}))))
 
-(defn update-velocities [moons]
-  (reduce (fn [coll moon]
-            (let [new-vel (mapv + (:velocity moon) (velocity-deltas (:position moon) (map :position moons)))]
-              (conj coll {:velocity new-vel :position (mapv + (:position moon) new-vel)})))
-          []
-          moons))
-
 (defn attract [this other]
   (cond (= this other) 0
         (< this other) 1
@@ -20,9 +13,16 @@
 
 (defn velocity-deltas [moon moons]
   (apply mapv (fn [this & others]
-               (reduce (fn [sum current] (+ sum (attract this current))) 0 others))
+                (reduce (fn [sum current] (+ sum (attract this current))) 0 others))
          moon
          moons))
+
+(defn update-velocities [moons]
+  (reduce (fn [coll moon]
+            (let [new-vel (mapv + (:velocity moon) (velocity-deltas (:position moon) (map :position moons)))]
+              (conj coll {:velocity new-vel :position (mapv + (:position moon) new-vel)})))
+          []
+          moons))
 
 (defn energy [moons]
   (reduce (fn [sum {:keys [position velocity]}]
@@ -31,4 +31,26 @@
           0
           moons))
 
+;;; part 1
 (energy (last (take 1001 (iterate update-velocities moons))))
+
+;;; part 2
+(defn just-dimension [moons idx]
+  (mapv (fn [moon]
+          (-> moon
+              (update :position #(nth % idx))
+              (update :velocity #(nth % idx)))) moons))
+
+(defn gcd [a b] (if (zero? b) a (recur b (mod a b))))
+(defn lcm [a b] (/ (* a b) (gcd a b)))
+(defn lcmv [& v] (reduce lcm v))
+
+(apply lcmv
+       (map (fn [idx]
+              (loop [moons* moons
+                     iteration 0]
+                (if (and (not= iteration 0) (= (just-dimension moons idx) (just-dimension moons* idx)))
+                  iteration
+                  (recur (update-velocities moons*) (inc iteration)))))
+            [0 1 2]))
+
--- README.md
+++ README.md
@@ -12,6 +12,7 @@
 |    9 | [1 + 2](2019/09.clj) • Clojure                      |
 |   10 | [1 + 2](2019/10.clj) • Clojure                      |
 |   11 | [1 + 2](2019/11.clj) • Clojure                      |
+|   12 | [1 + 2](2019/12.clj) • Clojure                      |
 
 # Advent of Code 2018
 
