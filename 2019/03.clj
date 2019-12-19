08c44f389bcc252dbef865b89f91157fc1519ce2
FelipeCortez <felipecortezfi@gmail.com>
2019-12-18 21:10:32 -0300

    Day 3, part 2
    FINALLY

--- 2019/03.clj
+++ 2019/03.clj
@@ -14,6 +14,7 @@
   {:dir   (keyword (subs segment-str 0 1))
    :delta (Integer/parseInt (subs segment-str 1))})
 
+
 (def wires (->> (slurp "03.in")
                 str/split-lines
                 (mapv split-commas)))
@@ -21,16 +22,19 @@
 (defn relative->absolute [segments]
   (loop [pos      {:x 0 :y 0}
          segments segments
-         result   {:h [], :v []}]
+         result   {:h [], :v []}
+         sum      0]
     (if-not (seq segments)
       result
       (let [segment   (first segments)
             [coord f] (delta-ops (:dir segment))
-            new-pos (merge-with f pos {coord (:delta segment)})]
+            new-pos (merge-with f pos {coord (:delta segment)})
+            new-sum (+ sum (Math/abs (- (coord new-pos) (coord pos))))]
         (recur new-pos
                (rest segments)
                (update result (coord {:x :h, :y :v})
-                       #(conj % [(pos (other-coord coord)) (map coord [pos new-pos])])))))))
+                       #(conj % [(pos (other-coord coord)) (map coord [pos new-pos]) sum]))
+               new-sum)))))
 
 (defn between? [from to n]
   (let [[from to] (sort [from to])]
@@ -43,8 +47,6 @@
        (between? from1 to1 fixed2)
        (between? from2 to2 fixed1)
        {:x fixed2 :y fixed1}))
-
-(crosses? [0 [0 5]] [3 [0 3]])
 
 (defn overlaps?
   [segment1 segment2]
@@ -60,9 +62,20 @@
 (def segments (mapv (fn [wire] (relative->absolute (mapv segment-str->segment wire)))
                     wires))
 
+;;; part 1
 (apply min (map (fn [{:keys [x y]}] (+ (Math/abs x) (Math/abs y)))
-                (let [[{hs1 :h vs1 :v} {hs2 :h vs2 :v}] segments]
-                  (concat (for [h hs1 v vs2 :let [crosses (crosses? h v)] :when crosses]
-                            crosses)
-                          (for [h hs2 v vs1 :let [crosses (crosses? h v)] :when crosses]
-                            crosses)))))
+                (let [[{hs1 :h vs1 :v} {hs2 :h vs2 :v}] segments
+                      crossings (fn [hs vs] (for [h hs v vs :let [crosses (crosses? h v)] :when crosses]
+                                              crosses))]
+                  (concat (crossings hs1 vs2) (crossings hs2 vs1)))))
+
+;;; part 2
+(defn crossings-delay [hs vs]
+  (for [[fixed1 [from1 to1] sum1 :as h] hs
+        [fixed2 [from2 to2] sum2 :as v] vs
+        :let [crosses (crosses? h v)] :when crosses]
+    (+ sum1 (Math/abs (- from1 fixed2))
+       sum2 (Math/abs (- from2 fixed1)))))
+
+(apply min (let [[{hs1 :h vs1 :v} {hs2 :h vs2 :v}] segments]
+             (concat (crossings-delay hs1 vs2) (crossings-delay hs2 vs1))))
