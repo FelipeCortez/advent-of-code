e1865ca2701c15ce48978c8581270067710fac7d
FelipeCortez <felipecortezfi@gmail.com>
2021-12-11 14:41:30 -0300

    2021, day 9, part 2

--- 2021/09.clj
+++ 2021/09.clj
@@ -1,3 +1,5 @@
+(ns smoke-basin)
+
 (def example "2199943210
 3987894921
 9856789892
@@ -13,18 +15,34 @@
 
 (defn at [coords [x y]] (-> coords (get y) (get x)))
 
-(let [max-x (count (first coords))
-      max-y (count coords)
-      deltas [[0 1] [0 -1] [1 0] [-1 0]]
-      valid? (fn [[x y]]
-               (and (<= 0 x (dec max-x))
-                    (<= 0 y (dec max-y))))]
-  (reduce +
-          (for [y (range max-y)
-                x (range max-x)
-                :let [v (at coords [x y])]
-                :when (->> (map v+ deltas (repeat [x y]))
-                           (filterv valid?)
-                           (mapv (partial at coords))
-                           (every? (partial < v)))]
-            (inc v))))
+(def deltas [[0 1] [0 -1] [1 0] [-1 0]])
+
+(defn fill! [coords valid? visited* point]
+  (vswap! visited* conj point)
+  (run! (partial fill! coords valid? visited*)
+        (filter (every-pred valid?
+                            (complement @visited*)
+                            (comp (partial not= 9) (partial at coords)))
+                (map v+ deltas (repeat point))))
+  @visited*)
+
+(let [max-x    (count (first coords))
+      max-y    (count coords)
+      valid?   (fn [[x y]]
+                 (and (<= 0 x (dec max-x))
+                      (<= 0 y (dec max-y))))
+      low-points
+      (for [y     (range max-y)
+            x     (range max-x)
+            :let  [v (at coords [x y])]
+            :when (->> (map v+ deltas (repeat [x y]))
+                       (filterv valid?)
+                       (mapv (partial at coords))
+                       (every? (partial < v)))]
+        [x y])]
+  [(reduce + (map (comp inc (partial at coords)) low-points))
+   (->> low-points
+       (map (fn [origin] (count (fill! coords valid? (volatile! #{}) origin))))
+       (sort #(compare %2 %1))
+       (take 3)
+       (reduce *))])
