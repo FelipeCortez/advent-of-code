a6fa3c5e50b97e3eed30583ff2f56ce6d0703d73
Felipe Cortez <github@felipecortez.net>
2022-12-21 23:42:28 -0300

    2022, day 21, part 2

--- 2022/21.clj
+++ 2022/21.clj
@@ -8,22 +8,39 @@
       {monkey (parse-long n)}
       {monkey [(eval (read-string op)) dep1 dep2]})))
 
-(let [monkeys->ops
+
+(defn simulate [initial]
+  (let [monkeys->ops
         (->> (slurp "2022/21.in")
              (str/split-lines)
              (map parse-line)
              (into {}))
 
+        monkeys->ops
+        (if initial
+          (assoc monkeys->ops "humn" initial)
+          monkeys->ops)
+
         promises (zipmap (keys monkeys->ops)
                          (repeatedly promise))]
-    (doseq [[monkey n] (filter (comp number? val) monkeys->ops)]
+    (doseq [[monkey n]
+            (filter (comp number? val) monkeys->ops)]
       (deliver (get promises monkey) n))
 
-    (doseq [[monkey [op dep1 dep2]] (filter (comp not number? val)
-                                            monkeys->ops)]
+    (doseq [[monkey [op dep1 dep2]]
+            (filter (comp not number? val) monkeys->ops)]
       (future
-        (println dep1 dep2)
         (deliver (get promises monkey)
-                       (op @(get promises dep1)
-                           @(get promises dep2)))))
-    @(get promises "root"))
+                 (op @(get promises dep1)
+                     @(get promises dep2)))))
+    [@(get promises "root")
+
+     (- @(get promises "fglq")
+        @(get promises "fzbp"))]))
+
+;; part 1
+(first (simulate nil))
+
+;; part 2
+(map (juxt identity (comp second simulate))
+     (take 100 (iterate (partial + 1) 3.5876475628E12))) ;; (zoomed in manually)
