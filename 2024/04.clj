2e0de4dcd9a9479a993a0ebde0cb19f608c91fe0
Felipe Cortez <github@felipecortez.net>
2024-12-04 11:25:33 -0300

    2024, day 4

---
+++ 2024/04.clj
@@ -0,0 +1,33 @@
+(require '[clojure.string :as str])
+(def grid (str/split-lines (slurp "2024/04.in")))
+(def size (count grid))
+(defn in-bounds? [[i j]] (and (<= 0 i (dec size)) (<= 0 j (dec size))))
+(defn bounded-iterate [f x] (into [] (take-while in-bounds? (iterate f x))))
+(def e  (partial bounded-iterate (fn [[i j]] [i (inc j)])))
+(def s  (partial bounded-iterate (fn [[i j]] [(inc i) j])))
+(def sw (partial bounded-iterate (fn [[i j]] [(inc i) (inc j)])))
+(def se (partial bounded-iterate (fn [[i j]] [(inc i) (dec j)])))
+(def t-edge (e [0 0]))
+(def l-edge (s [0 0]))
+(def tl-edges (into #{} (concat t-edge l-edge)))
+(def rb-edges (concat (e [0 0]) (s [0 (dec size)])))
+(def lines
+   (let [to (into #{} cat [(mapv sw tl-edges) (mapv se rb-edges) (mapv e l-edge) (mapv s t-edge)])
+         fro (mapv rseq to)]
+     (into #{} cat [to fro])))
+
+(defn char-at [pos] (get-in grid pos))
+(def line->word #(apply str (map char-at %)))
+(def count-xmas #(count (re-seq #"XMAS" %)))
+(reduce + (map (comp count-xmas line->word) lines))
+
+(count
+ (for [i (range 1 (dec size))
+       j (range 1 (dec size))
+       :when (and (= (char-at [i j]) \A)
+                  (= #{#{\M \S}}
+                     (set [(set [(char-at [(inc i) (dec j)])
+                                 (char-at [(dec i) (inc j)])])
+                           (set [(char-at [(dec i) (dec j)])
+                                 (char-at [(inc i) (inc j)])])])))]
+   [i j]))
